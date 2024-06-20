464a5fe8e52da31b77bf199711fc2268cb23e733
Oscar Zeballos <oscarzeballoscarvallo@gmail.com>
2024-06-20 12:56:13 -0400

    ejercicio nuevo estatico

--- resources/views/ejerciciosIA/martillo.blade.php
+++ resources/views/ejerciciosIA/martillo.blade.php
@@ -25,12 +25,48 @@
     </div>
 
     <br>
-    <a href="{{ route('tracking') }}" class="btn btn-primary">Comenzar</a>
+
+    <button onclick="startVideo()" class="btn btn-primary">Empezar</button>
+    <img id="video" src="" width="640" height="480" style="display: none;">
+
+    <div>¿Finalizó las repeticiones con exito?</div>
+    <button onclick="" class="btn btn-primary">Sí</button>
+    <button onclick="showForm()" class="btn btn-primary">No</button>
+    <div id="form-container" style="display: none;">
+        <h3>Retroalimentación</h3>
+        <form action="/ejerciciosIA/martillo/fail" method="POST">
+            @csrf
+            <div class="form-group col-md-6">
+                <label for="repeticiones">Cuantas repeticiones realizaste:</label>
+                <input type="number" class="form-control" id="repeticiones" name="repeticiones" required>
+            </div>
+            <div class="form-group col-md-6">
+                <label for="motivo">Motivo:</label>
+                <textarea class="form-control" id="motivo" name="motivo" required></textarea>
+            </div>
+            <br>
+            <button type="submit" class="btn btn-primary">Enviar</button>
+        </form>
+    </div>
     
+    <script>
+        function showForm() {
+            var formContainer = document.getElementById('form-container');
+            formContainer.style.display = 'block';
+        }
+    </script>
+    <script>
+        function startVideo() {
+            var videoFeed = document.getElementById('video');
+            videoFeed.style.display = 'block';
+            videoFeed.src = 'http://localhost:5000/calentamiento';
+        }
+    </script>
+
 
     <!-- Mostrar la salida del código Python en la página -->
-    <div id="output">
+    {{-- <div id="output">
         {{ $output ?? '' }}
-    </div>
+    </div> --}}
 
 @endsection
