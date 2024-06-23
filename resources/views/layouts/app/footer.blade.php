256e7dd25a176e03a8cad69d8ef12993549f982f
Oscar Zeballos <oscarzeballoscarvallo@gmail.com>
2024-06-22 20:11:47 -0400

    cambios de estilos

--- resources/views/layouts/app/footer.blade.php
+++ resources/views/layouts/app/footer.blade.php
@@ -3,22 +3,19 @@
         <div class="row text-muted">
             <div class="col-6 text-start">
                 <p class="mb-0">
-                    <a class="text-muted" href="https://adminkit.io/" target="_blank"><strong>AdminKit</strong></a> - <a class="text-muted" href="https://adminkit.io/" target="_blank"><strong>Bootstrap Admin Template</strong></a>								&copy;
+                    <a class="text-muted"  target="_blank"><strong>FisioSport</strong></a>
                 </p>
             </div>
             <div class="col-6 text-end">
                 <ul class="list-inline">
                     <li class="list-inline-item">
-                        <a class="text-muted" href="https://adminkit.io/" target="_blank">Support</a>
+                        <a class="text-muted" target="_blank">Copyrigth - 2024</a>
                     </li>
                     <li class="list-inline-item">
-                        <a class="text-muted" href="https://adminkit.io/" target="_blank">Help Center</a>
+                        <a class="text-muted" target="_blank">Privacy</a>
                     </li>
                     <li class="list-inline-item">
-                        <a class="text-muted" href="https://adminkit.io/" target="_blank">Privacy</a>
-                    </li>
-                    <li class="list-inline-item">
-                        <a class="text-muted" href="https://adminkit.io/" target="_blank">Terms</a>
+                        <a class="text-muted" target="_blank">Terms</a>
                     </li>
                 </ul>
             </div>
