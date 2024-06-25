77c27809d15ef0735c901a433396c5e7f9f97fe5
Oscar Zeballos <oscarzeballoscarvallo@gmail.com>
2024-06-25 17:36:10 -0400

    agregue envio de mensajes

--- resources/views/layouts/app/navPaciente.blade.php
+++ resources/views/layouts/app/navPaciente.blade.php
@@ -1,5 +1,4 @@
-
-              <!--   <ul id="rutinas" class="sidebar-dropdown list-unstyled collapse" data-bs-parent="#sidebar"
+<!--   <ul id="rutinas" class="sidebar-dropdown list-unstyled collapse" data-bs-parent="#sidebar"
                     style="">
                     <li class="sidebar-item"><a class="sidebar-link" href="{{ route('martillo') }}">Ejercicio 1</a></li>
                 </ul>
@@ -46,29 +45,29 @@
  -->
 
 <nav class="sidebar">
-        <header>
-            <div class="image-text">
-                <div class="text logo-text">
-                    <span class="name">FisioSport</span>
-                </div>
+    <header>
+        <div class="image-text">
+            <div class="text logo-text">
+                <span class="name">FisioSport</span>
             </div>
-        </header>
+        </div>
+    </header>
 
-        <div class="menu-bar">
-            <div class="menu">
-                <li class="search-box">
-                    Opciones
+    <div class="menu-bar">
+        <div class="menu">
+            <li class="search-box">
+                Opciones
+            </li>
+
+            <ul class="menu-links">
+
+                <li class="nav-link">
+                    <a href="{{ route('paciente_rutina') }}">
+                        <i class='bx bx-dumbbell icon'></i>
+                        <span class="text nav-text">Mis Rutinas</span>
+                    </a>
                 </li>
-
-                <ul class="menu-links">
-                    
-                   <li class="nav-link">
-                        <a href="{{ route('paciente_rutina') }}">
-                            <i class='bx bx-dumbbell icon'></i>
-                            <span class="text nav-text">Mis Rutinas</span>
-                        </a>
-                    </li>
-                    <li class="nav-link">
+                <li class="nav-link">
                     <a href="#" class="toggle-link" data-target="#ejercicios">
                         <i class='bx bx-run icon'></i>
                         <span class="text nav-text">Ejercicios</span>
@@ -81,27 +80,41 @@
                             </a>
                         </li>
                     </ul>
+
                 </li>
 
-                </ul>
-            </div>
+                <li class="nav-link">
+                    <a href="{{ route('crear_notificacion_paciente') }}">
+                        <i class='bx bxs-message-square-dots icon'></i>
+                        <span class="text nav-text">Enviar mensaje</span>
+                    </a>
+                </li>
 
-            <div class="bottom-content">
-                    <!-- Añadir contenido adicional aquí si es necesario -->
-            </div>
+                <li class="nav-link">
+                    <a href="{{ route('ver_mensajes_paciente', ['id' => auth()->user()->id]) }}">
+                        <i class='bx bxs-chat icon'></i>
+                        <span class="text nav-text">Ver mensajes</span>
+                    </a>
+                </li>
+
+            </ul>
         </div>
-    </nav>
+
+        <div class="bottom-content">
+            <!-- Añadir contenido adicional aquí si es necesario -->
+        </div>
+    </div>
+</nav>
 
 <script>
     document.addEventListener('DOMContentLoaded', function() {
-    var toggleLinks = document.querySelectorAll('.toggle-link');
-    toggleLinks.forEach(function(link) {
-        link.addEventListener('click', function(event) {
-            event.preventDefault();
-            var target = document.querySelector(this.getAttribute('data-target'));
-            this.parentElement.classList.toggle('active');
+        var toggleLinks = document.querySelectorAll('.toggle-link');
+        toggleLinks.forEach(function(link) {
+            link.addEventListener('click', function(event) {
+                event.preventDefault();
+                var target = document.querySelector(this.getAttribute('data-target'));
+                this.parentElement.classList.toggle('active');
+            });
         });
     });
-});
-
 </script>
